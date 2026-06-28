2c19f22538c174937ee14edcb2f06e04af8a883b
lugizmo <29488895+lugizmo@users.noreply.github.com>
2026-06-28 16:31:43 +0200

    test(fix): pass google test arguments to init of gtest lib

--- lib/test/main.cpp
+++ lib/test/main.cpp
@@ -6,8 +6,8 @@
 
 #include "gtest/gtest.h"
 
-auto main() -> int
+auto main(int argc, char** argv) -> int
 {
-    testing::InitGoogleTest();
+    testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
